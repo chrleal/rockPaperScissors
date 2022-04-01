9704c99caf40980eb64eac9d0052215e81030ac1
chrleal <chr.leal@gmail.com>
2022-04-01 13:17:46 -0300

    Add Function playRound

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 function computerPlay() {
+    let result;
     let number = Math.floor((Math.random() * 3) + 1);
     if (number == 1) {
-        console.log("Pedra");
+        result = "Rock";
     } else if (number == 2) {
-        console.log("Papel");
+        result = "Paper";
     } else {
-        console.log("Tesoura");
+        result = "Scissors";
     }
+    return result
 }
+function playRound(playerSelection, computerSelection){
+ let player = playerSelection.toLowerCase();
+    computerSelection = computerPlay()
+    let computer = computerSelection.toLowerCase();
+    if (player === computer) {
+        console.log("It's a draw!");   
+        console.log("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+    } else {
+        if (player === "rock"){
+            if (computer === "scissors") {
+                console.log("Rock beats Scissors! You Win! :D");
+                console.log("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            } else {
+                console.log("Paper beats Rock! You Lose! :(");
+                console.log("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            }
+        } else if (player === "paper"){
+            if (computer === "rock") {
+                console.log("Paper beats Rock! You Win! :D");
+                console.log("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            } else {
+                console.log("Scissor beats Paper! You Lose! :(");
+                console.log("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            }
+        } else if (player === "scissors"){
+            if (computer === "paper") {
+                console.log("Scissor beats Paper! You Win! :D");
+                console.log("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            } else {
+                console.log("Rock beats Scissors! You Lose! :(");
+                console.log("Your Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            }
+        }
+    }    
+    const playerChoice = "ROCK";
+    playRound(playerChoice)
 
-computerPlay()
