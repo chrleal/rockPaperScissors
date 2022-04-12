ca5f9f9b0df33ae3eafac20386cc3c759306b8b7
chrleal <chr.leal@gmail.com>
2022-04-12 18:16:08 -0300

    add simple UI add new functions

--- script.js
+++ script.js
@@ -13,69 +13,112 @@ function computerPlay() {
 }
 
 // function that plays a single round of Rock Paper Scissors
+let round = '';
 function playRound(playerSelection, computerSelection){
+    let text = ''
     let player = playerSelection.toLowerCase();
     computerSelection = computerPlay()
     let computer = computerSelection.toLowerCase();
     if (player === computer) {
-        round = 'draw'
-        return("It's a draw!\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection)
+        round = 'draw';
+        text = ("It's a draw!\n\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
     } else if (player === "rock"){
         if (computer === "scissors") {
-            round = 'win'
-            return("You Win! Rock beats Scissors! :D\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            round = 'win';
+            text = "You Win! Rock beats Scissors!\n\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection;
         } else {
-            round = 'lose'
-            return("You Lose! Paper beats Rock! :(\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            round = 'lose';
+            text = "You Lose! Paper beats Rock!\n\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection;
         }
     } else if (player === "paper"){
         if (computer === "rock") {
-            round = 'win'
-            return("You Win! Paper beats Rock! :D\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            round = 'win';
+            text = "You Win! Paper beats Rock!\n\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection;
         } else {
-            round = 'lose'
-            return("You Lose! Scissor beats Paper! :(\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            round = 'lose';
+            text = "You Lose! Scissor beats Paper!\n\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection;
         }
     } else if (player === "scissors"){
         if (computer === "paper") {
-            round = 'win'
-            return("You Win! Scissor beats Paper! :D\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            round = 'win';
+            text = "You Win! Scissor beats Paper!\n\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection;
         } else {
-            round = 'lose'
-            return("You Lose! Rock beats Scissors! :(\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection);
+            round = 'lose';
+            text = "You Lose! Rock beats Scissors!\n\nYour Choice: " + playerSelection + ". " + "Computer: " + computerSelection;
         }
     } else {
-        round = 'error'
-        return("Something went wrong. Please type only: Rock, Paper or Scissors")
-        }
+        round = 'error';
+        text = "Something went wrong. Please type only: Rock, Paper or Scissors";
+        } 
+    display.textContent = text;
+    return display.textContent
+    
  }    
 
 // function that plays a 5 round game, keeps score and reports a winner or loser at the end
-function game(){
-    let win = 0;
-    let lose = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Choose: Rock, Paper or Scissors. " + (i+1) + " out of 5.")
-        let result = playRound(playerSelection);
-        console.log(result) 
+let win = 0;
+let lose = 0;
+function game(playerGame){
+    let result = playRound(playerGame);
+    console.log(result)
 
-        if (round == 'win') {
-            win++;
-            console.log("Score: \nPlayer = " + win + ".\nComputer = " + lose + ".")
-        } else if (round == 'lose') {
-            lose++
-            console.log("Score: \nPlayer = " + win + ".\nComputer = " + lose + ".")
-        }
-
-    }
-
-    if (win > lose) {
-        console.log("Congratulations! You Win!")
-    } else if (lose > win) {
-        console.log("You lose! Try again.")
+    if (round == 'win') {
+        win++;
+        score.textContent = "Score: \nPlayer = " + win + ".\nComputer = " + lose + ".";
+    } else if (round == 'lose') {
+        lose++
+        score.textContent = "Score: \nPlayer = " + win + ".\nComputer = " + lose + ".";
     } else {
-        console.log("It's a Draw!")
+        score.textContent = "Score: \nPlayer = " + win + ".\nComputer = " + lose + ".";
+    } 
+    
+    if (win == 5 || lose == 5) {
+    testRound(); 
     }
 }
-let round;
-game();
+
+function clickButton(button) {
+    let playerGame = '';
+    if (button.className === 'rock') {
+        playerGame = 'Rock';
+    } else if (button.className === 'paper') {
+        playerGame = 'Paper';
+    } else {
+        playerGame = 'Scissors';
+    }
+    game(playerGame);
+}
+
+function testRound() {
+    if (win > lose) {
+        congrats.textContent = ("Congratulations! You Win!");
+    } else if (lose > win) {
+        congrats.textContent = ("You lose! Try again.");
+    } else {
+        congrats.textContent = ("It's a Draw!");
+    }
+}
+
+const play = document.querySelector('.play');
+const buttons = document.querySelectorAll('button');
+        buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (win >= 5 || lose >= 5) {
+                return;
+            } else {
+                clickButton(button);
+            }
+        });
+        });
+
+const display = document.querySelector('.display');
+display.setAttribute('style', 'margin-top: 5px; white-space: pre-line;');
+
+const score = document.querySelector('.score');
+score.setAttribute('style', 'margin-top: 5px; white-space: pre-line;');
+
+const body = document.querySelector('body');
+
+const congrats = document.createElement('div');
+congrats.classList.add('congrats');
+body.appendChild(congrats);
